b9197c1a4059f92e4f1f5e48abd8df90ced26ba4
j-dabling <jgdabble@zoho.com>
2022-10-06 12:34:42 -0600

    Added field of view.

--- Characters/AI - Soldier/Carl.gd
+++ Characters/AI - Soldier/Carl.gd
@@ -18,6 +18,7 @@ var weights = [0.1, 0.1, 0.1, 10, 0.1]
 var active_state : int = idle
 
 var proximity = []
+var fov = []
 var active_objective = Vector3.ZERO
 
 
@@ -25,6 +26,7 @@ func _process(delta) -> void:
 # Run every frame of the game.
 # Evaluates conditions and weighs the state machine accordingly.
 	_check_proximity()
+	var closest_body = _check_fov()
 
 	# Determines the active state from the current weights.
 	var heaviest_state = -1
@@ -35,11 +37,14 @@ func _process(delta) -> void:
 	#print("Carl's active state is: ", active_state)
 	match active_state:
 		advance:
-			move(self.transform.origin - active_objective.transform.origin) 
+			var advance_vector = _face_towards(active_objective)
+			move(delta, advance_vector) 
 		combat:
-			pass
+			head.look_at(closest_body.transform.origin, Vector3.UP)
 		retreat:
-			var retreat_vector = face_towards(proximity[0].transform.origin) * delta
+			# Future implimentations may involve additional logic to face
+			# the enemy and move backwards, or full turn and run.
+			var retreat_vector = _face_towards(proximity[0].transform.origin)
 			move(delta, retreat_vector)
 		cover:
 			pass
@@ -47,12 +52,18 @@ func _process(delta) -> void:
 			move(delta)
 
 
-func face_towards(focus: Vector3):
+func _face_towards(focus: Vector3):
 # Will orient the body and head to face towards 'focus'.
 # Returns the resulting direction vector.
 	var direction = focus.direction_to(self.transform.origin)
-	self.rotation.y = direction.x
-	head.rotation.z = -1 * direction.y
+	# self.rotation.y = direction.x
+	# head.rotation.z = -(direction.y)
+	
+#	var y_rot = self.transform.origin.angle_to(direction)
+#	var z_rot = self.transform.origin.angle_to(direction)
+#	self.rotation.y = direction.x
+#	head.rotation.z = -(direction.y)
+	head.look_at(focus, Vector3.UP)
 	return direction
 
 
@@ -70,6 +81,21 @@ func _check_proximity() -> KinematicBody:
 			closest_body = body
 			closest_dist = distance
 	status[retreat] = proximity_sum * weights[retreat]
+	return closest_body
+
+
+func _check_fov() -> KinematicBody:
+# Goes over every body in 'fov' and changes the combat state status accordingly.
+# Returns the closest body in FOV.
+	status[combat] = len(fov)
+	var closest_body = null
+	var closest_dist = 1000
+	for body in fov:
+		var body_dist = self.translation.distance_to(body.translation)
+		status[combat] += abs(body_dist - weights[combat])
+		if body_dist < closest_dist:
+			closest_body = body
+			closest_dist = body_dist
 	return closest_body
 
 
@@ -109,3 +135,13 @@ func _on_Proximity_body_entered(body):
 func _on_Proximity_body_exited(body):
 	if body.is_in_group("player") or body.is_in_group("bot"):
 		proximity.erase(body)
+
+
+func _on_Field_of_View_body_entered(body:Node):
+	if body.is_in_group("player") or body.is_in_group("bot"):
+		fov.append(body)
+
+
+func _on_Field_of_View_body_exited(body:Node):
+	if body.is_in_group("player") or body.is_in_group("bot"):
+		fov.erase(body)
